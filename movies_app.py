3b19662e4947503700496f303c7c3e28ad29d8e5
Satya Prakash Nanda <spnanda2004@gmail.com>
2024-06-27 06:06:33 +0530

    Add files via upload

---
+++ movies_app.py
@@ -0,0 +1,29 @@
+import pickle
+import streamlit as st
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+
+    for i in distances[1:11]:
+        # fetch the movie name
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('movie_list.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names = recommend(selected_movie)
+    for movie_name in recommended_movie_names:
+        st.write(movie_name)
